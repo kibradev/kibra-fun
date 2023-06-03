ccbcbb663b12f451ea52e5f3738ab526e4a6de34
kibra <62299912+kibradev@users.noreply.github.com>
2023-06-03 16:05:14 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,6 +1,29 @@
 SetCanAttackFriendly(PlayerPedId(), true, false)
 NetworkSetFriendlyFireOption(true)
 
+RegisterCommand('car', function(source, args, rawCommand)
+    -- argümanları kontrol eder
+    local player = PlayerId()
+    local carModel = args[1] or "adder" -- eğer bir argüman girilmezse varsayılan olarak "adder" aracını spawnlar
+
+    -- aracın modelini yükler
+    local modelHash = GetHashKey(carModel)
+    RequestModel(modelHash)
+    while not HasModelLoaded(modelHash) do
+        Wait(0)
+    end
+
+    -- aracı spawnlar
+    local playerPed = GetPlayerPed(player)
+    local playerCoords = GetEntityCoords(playerPed)
+    local car = CreateVehicle(modelHash, playerCoords, 0.0, true, false)
+    SetPedIntoVehicle(playerPed, car, -1)
+
+    -- modeli bellekten kaldırır
+    SetModelAsNoLongerNeeded(modelHash)
+end, false)
+
+
 local silahlar = {
 'weapon_dagger',
 'weapon_bat'   ,
